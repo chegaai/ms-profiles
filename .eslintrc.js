af06ca3efd2b1ea891e9ab01afb567f6e798345c
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2020-01-04 20:42:55 -0300

    Add eslint rule to allow spaces between brackets

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     '@typescript-eslint/strict-boolean-expressions': 'off',
     '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
     '@typescript-eslint/no-inferrable-types': 'warn',
-    '@typescript-eslint/explicit-function-return-type': 'off'
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'array-bracket-spacing': ['error', 'always']
   }
 }
